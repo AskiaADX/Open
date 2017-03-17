617e0f74a7db691bbef4e41faaa2640fe37a7e53
StephenPadawan <stephen@askia.com>
2017-03-17 12:53:56 +0100

    v2.0.0
    Added theme support, removed special email formatting

--- resources/dynamic/open.js
+++ resources/dynamic/open.js
@@ -5,13 +5,3 @@ currentcount({
     maxchar:{%=CurrentADC.PropValue("maxChar")%}
     
 });
-
-if({%= CurrentADC.PropValue("inputType")="email"%}){
-    concatEmail({
-    instanceId : {%= CurrentADC.InstanceID %},
-    inputId : "{%=CurrentQuestion.InputName()%}",
-    direction:"{%=CurrentADC.PropValue("counter")%}", 
-    maxchar:{%=CurrentADC.PropValue("maxChar")%},
-    emailvalidationmessage:"{%=CurrentADC.PropValue("emailValidationMessage")%}"
-	});
-}
--- resources/static/open.js
+++ resources/static/open.js
@@ -19,6 +19,9 @@ function currentcount(options){
             options.val = inputcontent.length;
             printcounter(options);
         }  
+        if (window.askia) {
+           askia.triggerAnswer();
+        }
     });   
     
 }
@@ -28,44 +31,5 @@ function printcounter(options){
     if (options.direction != "none"){
         counterdiv.innerHTML = options.val;
     }
-}
-
-function concatEmail(options){
-	document.getElementById(options.inputId+"_open1").addEventListener('change',function(){validateEmail(options);});
-    document.getElementById(options.inputId+"_open2").addEventListener('change',function(){validateEmail(options);});
-    document.getElementById(options.inputId+"_open3").addEventListener('change',function(){validateEmail(options);});
-}
-
-function validateEmail(options){
-        var box1 = document.getElementById(options.inputId+"_open1");
-        var box2 = document.getElementById(options.inputId+"_open2");
-        var box3 = document.getElementById(options.inputId+"_open3");
-        var emailaddress =  document.getElementById(options.inputId);
-    console.dir(box1);
-        emailaddress.value= box1.value + "@" + box2.value +"." + box3.value;
-        if((box1.value) && (box2.value) && (box3.value)){
-            options.address = emailaddress.value; 
-            options.adcSelector = '#adc_'+ options.instanceId;
-            var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;  
-            if(options.address.match(mailformat)){
-                options.val="";
-                    printcounter(options);
-                var inputs = document.querySelector(options.adcSelector + " .contentinput");
-                inputs.className = "emailwrapper contentinput";
-            }
-            else {
-               options.val=options.emailvalidationmessage;
-                console.dir(options);
-               printcounter(options);
-               var inputs = document.querySelector(options.adcSelector + " .contentinput");
-               inputs.className = "contentinput invalidemail";
-               /*var inputs = select3input();
-                inputs[0].className = "invalidemail";
-                inputs[1].className = "invalidemail";
-                inputs[2].className = "invalidemail";*/
-
-            }
-       }                              
-	return false
 }
 
