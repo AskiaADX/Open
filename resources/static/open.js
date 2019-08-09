c41f289944b98ab0d746f1abf8b7b4d6b0d36448
j4t1nb <j4t1nb@users.noreply.github.com>
2019-08-09 14:23:52 +0100

    v3.0.2
    v3.0.2 - Emulate maxLength when no browser validation

--- resources/static/open.js
+++ resources/static/open.js
@@ -38,3 +38,7 @@ function printcounter(options){
     }
 }
 
+function imposeMaxLength(Event, Object, MaxLen)
+{
+        return (Object.value.length < MaxLen)||(Event.keyCode == 8 ||Event.keyCode==46||(Event.keyCode>=35&&Event.keyCode<=40))
+}
