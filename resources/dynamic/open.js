af8ce1fc64add429dc2464bfe7e9efd12e959561
Prabath Gamage <prabath.gamage@platform1.net>
2019-08-22 20:20:43 +0530

    v3.0.3

--- resources/dynamic/open.js
+++ resources/dynamic/open.js
@@ -1,9 +1,10 @@
 currentcount({
     instanceId : {%= CurrentADC.InstanceID %},
     inputId : '{%=CurrentQuestion.InputName()%}',
-    direction:"{%=CurrentADC.PropValue("counter")%}", 
+    direction:"{%=CurrentADC.PropValue("counter")%}",
     maxchar:{%=CurrentADC.PropValue("maxChar")%},
     currentQuestion: '{%:= CurrentQuestion.Shortcut %}',
     showcongrats:{%=CurrentADC.PropValue("showCongrats")%},
-    suggestedchar:{%=CurrentADC.PropValue("suggestedChar").ToNumber()%}
+    suggestedchar:{%=CurrentADC.PropValue("suggestedChar").ToNumber()%},
+    usebrowservalidation: {%=CurrentADC.PropValue("useBrowserValidation")%}
 });
--- resources/static/open.js
+++ resources/static/open.js
@@ -29,6 +29,35 @@ function currentcount(options){
             askia.triggerAnswer();
         }
     });
+    
+    document.getElementById(options.inputId).addEventListener('paste',function(){
+      console.log('onpaste');
+      var inputcontent= this.value;
+      options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
+       options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
+
+       if(options.direction=='desc'){
+           options.val =  (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
+           printcounter(options);
+       }
+       else{
+           options.val = inputcontent.length;
+           printcounter(options);
+       }
+       if(options.suggestedchar >0 && options.showcongrats && inputcontent.length>=options.suggestedchar){
+          options.congratsdiv.style="display:block";
+        }
+          else{
+       options.congratsdiv.style="display:none";
+          }
+       if (window.askia
+           && window.arrLiveRoutingShortcut
+           && window.arrLiveRoutingShortcut.length > 0
+           && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
+           askia.triggerAnswer();
+       }
+
+    });
 
 }
 
