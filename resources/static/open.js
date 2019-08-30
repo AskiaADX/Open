46442c679b96cc73d3cfc1e8033e5840de2e052c
Prabath Gamage <prabath.gamage@platform1.net>
2019-08-30 11:36:14 +0530

    v3.0.4

--- resources/static/open.js
+++ resources/static/open.js
@@ -1,6 +1,7 @@
 function currentcount(options){
     options.adcSelector = '#adc_'+ options.instanceId;
-    document.getElementById(options.inputId).addEventListener('keyup',function(){
+
+    document.getElementById(options.inputId).addEventListener('keyup',function(e){
         //var inputcontent= this.value.replace(/\r(?!\n)|\n(?!\r)/g, '\r\n'); //handling of line-break characters
        var inputcontent= this.value;
        options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
@@ -29,7 +30,7 @@ function currentcount(options){
         }
     });
 
-    document.getElementById(options.inputId).addEventListener('input',function(){
+    document.getElementById(options.inputId).addEventListener('input',function(e){
       var inputcontent= this.value;
       options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
        options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
@@ -91,16 +92,31 @@ function printcounter(options){
         options.counterdiv.innerHTML = options.val;
     }
 }
-// imposeMaxLengthPasteEvent
 
+//onkeydown event
+function imposeMaxLengthOnKeydown(Event, Object, MaxLen)
+{
+  if ( (Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode==46 || (Event.keyCode>=35&&Event.keyCode<=40))) {
+      return true;
+  } else {
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      if (/android/i.test(userAgent)) //android detects
+        Object.value = (Object.value).substring(0,MaxLen - 1);
+
+      (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
+  }
+
+}
+
+//onkeypress event
 function imposeMaxLength(Event, Object, MaxLen)
 {
-	console.log("called");
     if ( (Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode==46 || (Event.keyCode>=35&&Event.keyCode<=40))) {
         return true;
     } else {
         (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
     }
+
 }
 
 //onpaste event
@@ -114,5 +130,4 @@ function imposeMaxLengthOnPaste(Event, Object, MaxLen){
   } else {
       return true;
   }
-
 }
