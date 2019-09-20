92b9e901c93eb945a5062dc2e137bb81f53690be
jeromed <jeromed@askia.com>
2019-09-20 11:11:27 +0200

    v3.0.5

--- resources/static/open.js
+++ resources/static/open.js
@@ -1,27 +1,27 @@
-function currentcount(options){
-    options.adcSelector = '#adc_'+ options.instanceId;
+function currentcount(options) {
+    options.adcSelector = '#adc_' + options.instanceId;
 
-    document.getElementById(options.inputId).addEventListener('keyup',function(e){
+    document.getElementById(options.inputId).addEventListener('keyup', function (e) {
         //var inputcontent= this.value.replace(/\r(?!\n)|\n(?!\r)/g, '\r\n'); //handling of line-break characters
-       var inputcontent= this.value;
-       options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
+        var inputcontent = this.value;
+        options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
         options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
 
 
-        if(options.direction=='desc'){
-            options.val =  (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
+        if (options.direction == 'desc') {
+            options.val = (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
             printcounter(options);
         }
-        else{
+        else {
             options.val = inputcontent.length;
             printcounter(options);
         }
-        if(options.suggestedchar >0 && options.showcongrats && inputcontent.length>=options.suggestedchar){
-           options.congratsdiv.style="display:block";
-         }
-           else{
-				options.congratsdiv.style="display:none";
-           }
+        if (options.suggestedchar > 0 && options.showcongrats && inputcontent.length >= options.suggestedchar) {
+            options.congratsdiv.style = "display:block";
+        }
+        else {
+            options.congratsdiv.style = "display:none";
+        }
         if (window.askia
             && window.arrLiveRoutingShortcut
             && window.arrLiveRoutingShortcut.length > 0
@@ -30,88 +30,90 @@ function currentcount(options){
         }
     });
 
-    document.getElementById(options.inputId).addEventListener('input',function(e){
-      var inputcontent= this.value;
-      options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
-       options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
+    document.getElementById(options.inputId).addEventListener('input', function (e) {
+        var inputcontent = this.value;
+        options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
+        options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
 
-       if(options.direction=='desc'){
-           options.val =  (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
-           printcounter(options);
-       }
-       else{
-           options.val = inputcontent.length;
-           printcounter(options);
-       }
-       if(options.suggestedchar >0 && options.showcongrats && inputcontent.length>=options.suggestedchar){
-          options.congratsdiv.style="display:block";
+        if (options.maxchar > 0 && options.maxchar - inputcontent.length < 0) {
+            this.value = this.value.substring(0, options.maxchar);
         }
-          else{
-       options.congratsdiv.style="display:none";
-          }
-       if (window.askia
-           && window.arrLiveRoutingShortcut
-           && window.arrLiveRoutingShortcut.length > 0
-           && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
-           askia.triggerAnswer();
-       }
+
+        if (options.direction == 'desc') {
+            options.val = (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
+            printcounter(options);
+        }
+        else {
+            options.val = inputcontent.length;
+            printcounter(options);
+        }
+        if (options.suggestedchar > 0 && options.showcongrats && inputcontent.length >= options.suggestedchar) {
+            options.congratsdiv.style = "display:block";
+        }
+        else {
+            options.congratsdiv.style = "display:none";
+        }
+        if (window.askia
+            && window.arrLiveRoutingShortcut
+            && window.arrLiveRoutingShortcut.length > 0
+            && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
+            askia.triggerAnswer();
+        }
     });
 
-    document.getElementById(options.inputId).addEventListener('paste',function(){
-      var inputcontent= this.value;
-      options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
-       options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
+    document.getElementById(options.inputId).addEventListener('paste', function () {
+        var inputcontent = this.value;
+        options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
+        options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
 
-       if(options.direction=='desc'){
-           options.val =  (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
-           printcounter(options);
-       }
-       else{
-           options.val = inputcontent.length;
-           printcounter(options);
-       }
-       if(options.suggestedchar >0 && options.showcongrats && inputcontent.length>=options.suggestedchar){
-          options.congratsdiv.style="display:block";
+        if (options.direction == 'desc') {
+            options.val = (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
+            printcounter(options);
         }
-          else{
-       options.congratsdiv.style="display:none";
-          }
-       if (window.askia
-           && window.arrLiveRoutingShortcut
-           && window.arrLiveRoutingShortcut.length > 0
-           && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
-           askia.triggerAnswer();
-       }
+        else {
+            options.val = inputcontent.length;
+            printcounter(options);
+        }
+        if (options.suggestedchar > 0 && options.showcongrats && inputcontent.length >= options.suggestedchar) {
+            options.congratsdiv.style = "display:block";
+        }
+        else {
+            options.congratsdiv.style = "display:none";
+        }
+        if (window.askia
+            && window.arrLiveRoutingShortcut
+            && window.arrLiveRoutingShortcut.length > 0
+            && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
+            askia.triggerAnswer();
+        }
 
     });
 
 }
 
-function printcounter(options){
-    if (options.direction != "none"){
+function printcounter(options) {
+    if (options.direction != "none") {
         options.counterdiv.innerHTML = options.val;
     }
 }
 
 //onkeydown event
-function imposeMaxLengthOnKeydown(Event, Object, MaxLen)
-{
-  if ( (Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode==46 || (Event.keyCode>=35&&Event.keyCode<=40))) {
-      return true;
-  } else {
-      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-      if (/android/i.test(userAgent)) //android detects
-        Object.value = (Object.value).substring(0,MaxLen - 1);
+function imposeMaxLengthOnKeydown(Event, Object, MaxLen) {
+    if ((Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode == 46 || (Event.keyCode >= 35 && Event.keyCode <= 40))) {
+        return true;
+    } else {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        if (/android/i.test(userAgent)) //android detects
+            Object.value = (Object.value).substring(0, MaxLen - 1);
 
-      (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
-  }
+        (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
+    }
 
 }
 
 //onkeypress event
-function imposeMaxLength(Event, Object, MaxLen)
-{
-    if ( (Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode==46 || (Event.keyCode>=35&&Event.keyCode<=40))) {
+function imposeMaxLength(Event, Object, MaxLen) {
+    if ((Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode == 46 || (Event.keyCode >= 35 && Event.keyCode <= 40))) {
         return true;
     } else {
         (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
@@ -120,14 +122,14 @@ function imposeMaxLength(Event, Object, MaxLen)
 }
 
 //onpaste event
-function imposeMaxLengthOnPaste(Event, Object, MaxLen){
-  let paste = (Event.clipboardData || window.clipboardData).getData('text');
-  var endP = (Object.value + paste);
-  if(endP.length >= MaxLen){
-    paste = endP.substring(0,MaxLen);
-    Object.value = paste;
-    (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
-  } else {
-      return true;
-  }
+function imposeMaxLengthOnPaste(Event, Object, MaxLen) {
+    let paste = (Event.clipboardData || window.clipboardData).getData('text');
+    var endP = (Object.value + paste);
+    if (endP.length >= MaxLen) {
+        paste = endP.substring(0, MaxLen);
+        Object.value = paste;
+        (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
+    } else {
+        return true;
+    }
 }
