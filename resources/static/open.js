d5dd74ce4ba59734ac61d78230660e6c9ab23060
Prabath Gamage <prabath.gamage@platform1.net>
2020-09-10 11:51:46 +0530

    Revert "v3.0.5 - custom error message - email inputs"
    This reverts commit c786df27855f877aa7f735cb0a4beff5e659d0f6.

--- resources/static/open.js
+++ resources/static/open.js
@@ -30,6 +30,32 @@ function currentcount(options){
         }
     });
 
+    document.getElementById(options.inputId).addEventListener('input',function(e){
+      var inputcontent= this.value;
+      options.counterdiv = document.querySelector(options.adcSelector + " .counterdiv .counter b");
+       options.congratsdiv = document.querySelector(options.adcSelector + " .congrats-message");
+
+       if(options.direction=='desc'){
+           options.val =  (options.maxchar - inputcontent.length > 0 ? options.maxchar - inputcontent.length : 0);
+           printcounter(options);
+       }
+       else{
+           options.val = inputcontent.length;
+           printcounter(options);
+       }
+       if(options.suggestedchar >0 && options.showcongrats && inputcontent.length>=options.suggestedchar){
+          options.congratsdiv.style="display:block";
+        }
+          else{
+       options.congratsdiv.style="display:none";
+          }
+       if (window.askia
+           && window.arrLiveRoutingShortcut
+           && window.arrLiveRoutingShortcut.length > 0
+           && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
+           askia.triggerAnswer();
+       }
+    });
 
     document.getElementById(options.inputId).addEventListener('paste',function(){
       var inputcontent= this.value;
@@ -65,4 +91,43 @@ function printcounter(options){
     if (options.direction != "none"){
         options.counterdiv.innerHTML = options.val;
     }
+}
+
+//onkeydown event
+function imposeMaxLengthOnKeydown(Event, Object, MaxLen)
+{
+  if ( (Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode==46 || (Event.keyCode>=35&&Event.keyCode<=40))) {
+      return true;
+  } else {
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      if (/android/i.test(userAgent)) //android detects
+        Object.value = (Object.value).substring(0,MaxLen - 1);
+
+      (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
+  }
+
+}
+
+//onkeypress event
+function imposeMaxLength(Event, Object, MaxLen)
+{
+    if ( (Object.value.length < MaxLen) || (Event.keyCode == 8 || Event.keyCode==46 || (Event.keyCode>=35&&Event.keyCode<=40))) {
+        return true;
+    } else {
+        (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
+    }
+
+}
+
+//onpaste event
+function imposeMaxLengthOnPaste(Event, Object, MaxLen){
+  let paste = (Event.clipboardData || window.clipboardData).getData('text');
+  var endP = (Object.value + paste);
+  if(endP.length >= MaxLen){
+    paste = endP.substring(0,MaxLen);
+    Object.value = paste;
+    (Event.preventDefault ? Event.preventDefault() : event.returnValue = false);
+  } else {
+      return true;
+  }
 }
