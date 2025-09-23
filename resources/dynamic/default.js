20d6e06555a5ebb91fc20f57eae7c65449a3cc98
TheBigWeeb <jordan.grindle@ipsos.com>
2025-09-23 10:01:16 -0400

    added prompt script

--- resources/dynamic/default.js
+++ resources/dynamic/default.js
@@ -1,3 +1,4 @@
+
 {%
  Dim strInputIds = ""
  Dim inputName  =  CurrentQuestion.InputName()
@@ -18,23 +19,55 @@
  EndIf
 %}
 
-$(window).on("load",function() {
-	$('#adc_{%= CurrentADC.InstanceId %}').adcOpen({
-     instanceId : {%= CurrentADC.InstanceID %},
-     inputId : '{%=CurrentQuestion.InputName()%}',
-     direction:"{%=CurrentADC.PropValue("counter")%}",
-     maxchar:{%=CurrentADC.PropValue("maxChar")%},
-     minchar:{%=CurrentADC.PropValue("minChar")%},
-     currentQuestion: '{%:= CurrentQuestion.Shortcut %}',
-     showcongrats:{%=CurrentADC.PropValue("showCongrats")%},
-     suggestedchar:{%=CurrentADC.PropValue("suggestedChar").ToNumber()%},
-     usebrowservalidation: {%=CurrentADC.PropValue("useBrowserValidation")%},
-     strExclusiveResponseIds : '{%=strInputIds %}',
-     isInLoop : {%= (CurrentADC.PropValue("isInLoop") = "1") %},
-     items : [
-       {% IF CurrentADC.PropValue("isInLoop") = "1" Then %}
-           {%:= CurrentADC.GetContent("dynamic/open_loop.js").ToText() %}
-       {% EndIF %}
-     ]
- });
+ 
+
+$(window).on("load", function() {
+    var $el = $('#adc_{%= CurrentADC.InstanceId %}');
+
+    $el.adcOpen({
+        instanceId: {%= CurrentADC.InstanceID %},
+        inputId: '{%=CurrentQuestion.InputName()%}',
+        direction: "{%=CurrentADC.PropValue("counter")%}",
+        maxchar: {%=CurrentADC.PropValue("maxChar")%},
+        minchar: {%=CurrentADC.PropValue("minChar")%},
+        currentQuestion: '{%:= CurrentQuestion.Shortcut %}',
+        showcongrats: {%=CurrentADC.PropValue("showCongrats")%},
+        suggestedchar: {%=CurrentADC.PropValue("suggestedChar").ToNumber()%},
+        usebrowservalidation: {%=CurrentADC.PropValue("useBrowserValidation")%},
+        strExclusiveResponseIds: '{%=strInputIds %}',
+        isInLoop: {%= (CurrentADC.PropValue("isInLoop") = "1") %},
+        items: [
+            {% IF CurrentADC.PropValue("isInLoop") = "1" Then %}
+                {%:= CurrentADC.GetContent("dynamic/open_loop.js").ToText() %}
+            {% EndIF %}
+        ]
+    });
+
+    $el.adcPrompt({
+        instanceId: {%= CurrentADC.InstanceID %},
+        inputId: '{%=CurrentQuestion.InputName()%}',
+        currentQuestion: '{%:= CurrentQuestion.Shortcut %}',
+        questionText: '{%=CurrentQuestion.LongCaption%}',
+        maxPrompts: {%=CurrentADC.PropValue("maxPrompts")%},
+        promptQuestion: "{%=CurrentADC.PropValue("promptQuestion")%}",
+        useAI: {%=CurrentADC.PropValue("useAI")%},
+        timePrompt: {%=CurrentADC.PropValue("timePrompts")%},
+        timeDelay: {%=CurrentADC.PropValue("timeDelay")%},
+        minChars: {%=CurrentADC.PropValue("minChars")%},
+        useSpace: {%=CurrentADC.PropValue("useSpace")%},
+        useEnd: {%=CurrentADC.PropValue("useEnd")%},        
+        promptArray:  [
+        {%
+          Dim i
+          Dim myQuestion = Survey.Questions.FindByShortcut(CurrentADC.PropValue("promptQuestion"))
+          For i = 1 To  myQuestion.Responses.Count 
+          %}"{%:= myQuestion.Responses[i].Caption %}" {%:= On(i <> myQuestion.Responses.Count, ",", "") %}
+          {%
+              Next
+          %}
+          ]
+    });
 });
+
+
+
